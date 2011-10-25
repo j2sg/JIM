30e67dd5801b967e4c92e627bfb7f89cda8c5594
Juan José Salazar García <jjslzgc@gmail.com>
2011-10-26 01:15:10 +0200

    I have forgotten to add the license preambule in the files invoiceeditor.h and invoiceeditor.cpp, issue resolved

--- include/view/invoiceeditor.h
+++ include/view/invoiceeditor.h
@@ -1,3 +1,23 @@
+/**
+ *  This file is part of QInvoicer.
+ *
+ *  Copyright (c) 2011 Juan Jose Salazar Garcia jjslzgc@gmail.com - https://github.com/j2sg/QInvoicer
+ *
+ *  QInvoicer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QInvoicer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QInvoicer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
 #ifndef INVOICEEDITOR_H
 #define INVOICEEDITOR_H
 
--- src/view/invoiceeditor.cpp
+++ src/view/invoiceeditor.cpp
@@ -1,3 +1,23 @@
+/**
+ *  This file is part of QInvoicer.
+ *
+ *  Copyright (c) 2011 Juan Jose Salazar Garcia jjslzgc@gmail.com - https://github.com/j2sg/QInvoicer
+ *
+ *  QInvoicer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  QInvoicer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QInvoicer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
 #include "invoiceeditor.h"
 #include <QtGui>
 #include <iostream>
@@ -7,8 +27,6 @@ View::InvoiceEditor::InvoiceEditor(Model::Domain::InvoiceType type, QWidget *par
 {
     createWidgets();
     setWindowTitle(tr("%1 Invoice").arg((type == Model::Domain::Sale) ? "Sale" : "Buy"));
-    setWindowIcon(QIcon(QString(":/images/%1invoice.png").
-                        arg((type == Model::Domain::Sale) ? "sale" : "buy")));
     setAttribute(Qt::WA_DeleteOnClose);
 }
 
